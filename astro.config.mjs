0bd6e3e53c5c608ef91c2f3b45c0231ce181aa30
Jan Čejka <posta@jancejka.cz>
2026-07-23 09:58:04 +0200

    Umami Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import umami from '@yeskunall/astro-umami';
 
 export default defineConfig({
   output: 'static',
   site: 'https://jancejka.cz',
-  integrations: [sitemap()],
+  integrations: [
+    sitemap(),
+    umami({
+        id: "94db1cb1-74f4-4a40-ad6c-962362670409",
+        endpointUrl: "https://analytics.rebma.cz"
+    })
+],
 });
